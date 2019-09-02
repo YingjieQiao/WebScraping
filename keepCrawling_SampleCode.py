b6917b607e939194009df6cb5084fd2214b1aa00
Yingjie Qiao <49013092+YingjieQiao@users.noreply.github.com>
2019-09-02 17:58:52 +0800

    Update keepCrawling_SampleCode.py

--- keepCrawling_SampleCode.py
+++ keepCrawling_SampleCode.py
@@ -57,3 +57,31 @@ def followExternalOnly(startingSite):
     
     
 followExternalOnly("http://oreilly.com")
+
+#Example function to crawl a certain website.
+'''
+def getLinks(articleUrl):
+    global pages
+    
+    html = urlopen("http://en.wikipedia.org"+articleUrl)
+    bsObj = BeautifulSoup(html, features="html.parser")
+    
+    try:
+        print(bsObj.h1.get_text())
+        print(bsObj.find(id ="mw-content-text").findAll("p")[0])
+        print(bsObj.find(id="ca-edit").find("span").find("a").attrs['href'])
+    except AttributeError:
+        print("This page is missing something!")
+        
+    for link in bsObj.findAll("a", href=re.compile("^(/wiki/)")):
+        if 'href' in link.attrs:
+            if link.attrs['href'] not in pages:
+                newPage = link.attrs['href']
+                print("-----------------------\n" + newPage)
+                pages.add(newPage)
+                getLinks(newPage)
+                
+                
+    
+links = getLinks("/wiki/Kevin_Bacon")
+'''
